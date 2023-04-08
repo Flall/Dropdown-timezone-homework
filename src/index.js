bdd5f337e9035846295b2279b217e3ed3542b541
Flall <119894671+Flall@users.noreply.github.com>
2023-04-08 18:58:01 +0200

    Code fixed for alert to show

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-let parisDateTimeInfo = moment()
-  .tz("Europe/Paris")
-  .format("dddd, MMMM D, YYYY hh:mm A");
-let parisDateTimeInfoElement = document.querySelector("#paris");
-parisDateTimeInfoElement.innerHTML = parisDateTimeInfo;
-
-let tokyoDateTimeInfo = moment()
-  .tz("Asia/Tokyo")
-  .format("dddd, MMMM D, YYYY hh:mm A");
-let tokyoDateTimeInfoElement = document.querySelector("#tokyo");
-tokyoDateTimeInfoElement.innerHTML = tokyoDateTimeInfo;
-
-let sydneyDateTimeInfo = moment()
-  .tz("Australia/Sydney")
-  .format("dddd, MMMM D, YYYY hh:mm A");
-let sydneyDateTimeInfoElement = document.querySelector("#sydney");
-sydneyDateTimeInfoElement.innerHTML = sydneyDateTimeInfo;
-
 function showTimeZoneInfo(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      alert(`It is currently ${parisDateTimeInfo} in Europe/Paris`);
-    }
-
-    if (event.target.value === "tokyo") {
-      alert(`It is currently ${tokyoDateTimeInfo} in Asia/Tokyo`);
-    }
-
-    if (event.target.value === "sydney") {
-      alert(`It is currently ${sydneyDateTimeInfo} in Asia/Tokyo`);
-    }
+    let currentDateTimeInfo = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY hh:mm A");
+    alert(`It is ${currentDateTimeInfo} in ${event.target.value}`);
   }
 }
 let countrySelection = document.querySelector("#countries");
